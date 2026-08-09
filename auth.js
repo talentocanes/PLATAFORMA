09438ab47217571215d4c9aacee2aa7bc2d1b9f3
talentocanes <fredy_a_diaz@hotmail.com>
2026-08-09 17:15:15 -0500

    Cambios

--- auth.js
+++ auth.js
@@ -1,4 +1,4 @@
-import { supabase, usernameToEmail } from './supabaseClient.js?v=9';
+import { supabase, usernameToEmail } from './supabaseClient.js?v=10';
 
 // ---------------------------------------------------------
 // Iniciar sesión con usuario y contraseña
--- theme.js
+++ theme.js
@@ -1,4 +1,4 @@
-import { supabase } from './supabaseClient.js?v=9';
+import { supabase } from './supabaseClient.js?v=10';
 
 // ---------------------------------------------------------
 // 8 paletas de color. La primera ("azul-original") es el tono
