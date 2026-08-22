be40c36ff892a0d09a4939d4272ef5776436d954
talentocanes <fredy_a_diaz@hotmail.com>
2026-08-22 10:28:13 -0500

    Cambios

--- auth.js
+++ auth.js
@@ -1,4 +1,4 @@
-import { supabase, usernameToEmail } from './supabaseClient.js?v=12';
+import { supabase, usernameToEmail } from './supabaseClient.js?v=15';
 
 // ---------------------------------------------------------
 // Iniciar sesión con usuario y contraseña
